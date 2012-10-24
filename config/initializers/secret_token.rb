9bb333f3d2810a766c51dafdfaf74cd7707309c0
higgisp <sam@isotope11.com>
2012-10-24 15:02:47 -0500

    Starting on the Dividend Reporter

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DividendReporter::Application.config.secret_token = '4870623a0569fba37633fa051cc31ee92e7d79f337b39b0736d5b7f120e5502c9d2c4d3fd23065ad87b44c3fcc5438eab46b309414d514b1c8a8ec1e9d201029'
